f20465e4955d26cd195c9b9f0af8a890dfb92448
BasamDawii <basa0033@easv365.dk>
2022-11-28 09:55:03 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-package gui;
+package src;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View/MovieView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("MyTunes");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
