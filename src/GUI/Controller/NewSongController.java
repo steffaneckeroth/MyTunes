e2d45de5cce1c835dd4a343778683f53d009a15c
steffaneckeroth <stefeckeroth@hotmail.com>
2022-11-30 21:18:58 +0100

    Ds

--- src/GUI/Controller/NewSongController.java
+++ src/GUI/Controller/NewSongController.java
@@ -1,13 +1,37 @@
 package src.GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class NewSongController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class NewSongController implements Initializable {
     public TextField txtFTtl;
+    public ComboBox cbxDropDown;
 
-    public void handleTxtTtl(ActionEvent actionEvent) {
 
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ComboBox<Object> comboBox = new ComboBox<>();
+        comboBox.getItems().removeAll(comboBox.getItems());
+        comboBox.getItems().addAll("Option A", "Option B", "Option C");
+        comboBox.getSelectionModel().select("Option B");
     }
-}
+
+
+        public void handleTxtTtl (ActionEvent actionEvent){
+
+
+        }
+
+        public void handleDropDown (ActionEvent actionEvent){
+
+        }
+    }
+
