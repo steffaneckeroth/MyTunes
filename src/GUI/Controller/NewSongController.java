9b38311c9a01e1d7001181f9d7cc4479729af214
BasamDawii <basa0033@easv365.dk>
2022-12-01 09:32:12 +0100

    Fixed

--- src/GUI/Controller/NewSongController.java
+++ src/GUI/Controller/NewSongController.java
@@ -9,42 +9,22 @@ import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-
-public class NewSongController implements Initializable {
-    public TextField txtFTtl;
-    public ComboBox cbxDropDown;
-
-
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class NewSongController {
+public class NewSongController  {
     public TextField txtFTtl;
-
-    public void handleTxtTtl(ActionEvent actionEvent)
-    {
+    public ComboBox cbxDropDown;
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        ComboBox<Object> comboBox = new ComboBox<>();
-        comboBox.getItems().removeAll(comboBox.getItems());
-        comboBox.getItems().addAll("Option A", "Option B", "Option C");
-        comboBox.getSelectionModel().select("Option B");
-    }
-
-
-        public void handleTxtTtl (ActionEvent actionEvent){
-
+        public void handleTxtTtl(ActionEvent actionEvent) {
 
         }
+    public void handleDropDown (ActionEvent actionEvent){
 
-        public void handleDropDown (ActionEvent actionEvent){
-
-        }
     }
 
+}
