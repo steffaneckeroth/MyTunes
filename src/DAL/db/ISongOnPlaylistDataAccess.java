ba048116e109f828772ed6f563be3bbfaef92a89
steffaneckeroth <stefeckeroth@hotmail.com>
2022-12-12 08:49:55 +0100

    Hjæppe

--- src/DAL/db/ISongOnPlaylistDataAccess.java
+++ src/DAL/db/ISongOnPlaylistDataAccess.java
@@ -1,10 +1,17 @@
 package src.DAL.db;
 
+import src.BE.Playlist;
 import src.BE.Song;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface ISongOnPlaylistDataAccess {
 
-    public Song addToPlaylist(String name, Song song)throws Exception;
+    public Song addToPlaylist(Playlist playlist, Song song)throws Exception;
 
 
+    List<Playlist> getAllSongOnPlaylists();
+
+    ArrayList<Song> getSongsOnPlaylist(Playlist playlist);
 }
--- src/GUI/Model/SongOnPlaylistModel.java
+++ src/GUI/Model/SongOnPlaylistModel.java
@@ -1,25 +1,42 @@
 package src.GUI.Model;
-/*
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import src.BE.Playlist;
 import src.BE.Song;
 import src.BLL.PlaylistManager;
 import src.BE.SongOnPlaylist;
+import src.BLL.SongOnPlaylistManager;
+
+import java.util.ArrayList;
 
 public class SongOnPlaylistModel {
-    private ObservableList<SongOnPLaylist> songOnPLaylistsToBeViewed;
+
+    private SongOnPlaylistManager songOnPlaylistManager;
+
+    private SongOnPlaylist selectedSongOnPlaylist;
+
+    public SongOnPlaylistModel() throws Exception {
+        songOnPlaylistManager = new SongOnPlaylistManager();
+        songOnPlaylistsToBeViewed = FXCollections.observableArrayList();
+        songOnPlaylistsToBeViewed.addAll((SongOnPlaylist) songOnPlaylistManager.getAllSongOnPlaylists());
+    }
+
+
+
+    private ObservableList<SongOnPlaylist> songOnPlaylistsToBeViewed;
 
 
     public void addToPlaylist(Playlist playlist, Song song) throws Exception {
         Song mSong = songOnPlaylistManager.addToPlaylist(playlist, song);
 
-        PlaylistManager.addToPlaylist(playlist, song);
+        PlaylistManager.addToPlaylist(playlist, mSong);
     }
 
 
-
-
+    public ArrayList<Song> getSongsOnPlaylist(Playlist playlist) {
+        return songOnPlaylistManager.getSongsOnPlaylist(playlist);
+    }
 }
 
 
- */
