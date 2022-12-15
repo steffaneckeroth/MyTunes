9d22b7c72e5427a63e1744ff85dcd1e195be7a00
brunstighanhval <mlkaer1@hotmail.com>
2022-12-15 14:25:06 +0100

    Comments for Bll

--- src/BLL/SongManager.java
+++ src/BLL/SongManager.java
@@ -14,28 +14,55 @@ public class SongManager {
 
     private ISongDataAccess songDAO;
 
-    public SongManager() {
-        songDAO = new SongDAO_DB();
-    }
+    public SongManager() {songDAO = new SongDAO_DB();}
 
-    public List<Song> getAllSongs() throws Exception {
-        return songDAO.getAllSong();
-    }
+    /**
+     * This code is a method that returns a list of songs using the songDAO object.
+     * @return songDAO.getAllSong()
+     * @throws Exception
+     */
+    public List<Song> getAllSongs() throws Exception {return songDAO.getAllSong();}
 
+    /**
+     * This method searches for songs using the provided query string.
+     * @param query
+     * @return searchResult
+     * @throws Exception
+     */
     public List<Song> searchSongs(String query) throws Exception {
         List<Song> allSong = getAllSongs();
         List<Song> searchResult = songSearcher.search(allSong, query);
         return searchResult;
     }
 
+    /**
+     * It creates a new song with the all the parameters down below
+     * @param title
+     * @param artist
+     * @param category
+     * @param filepath
+     * @param duration
+     * @return songDAO.createSong(title, artist, category, filepath, duration)
+     * @throws Exception
+     */
     public Song createNewSong(String title, String artist, String category, String filepath, Time duration) throws Exception {
         return songDAO.createSong(title, artist, category, filepath, duration);
     }
 
+    /**
+     * This method updates the songs in the database using the songDAO.
+     * @param updatedSongs
+     * @throws Exception
+     */
     public void updateSongs(Song updatedSongs) throws Exception {
         songDAO.updateSongs(updatedSongs);
     }
 
+    /**
+     * This method deletes the songs in the database using the songDAO.
+     * @param deletedSongs
+     * @throws Exception
+     */
     public void deleteSongs(Song deletedSongs) throws Exception{
         songDAO.deleteSongs(deletedSongs);
     }
--- src/BLL/SongOnPlaylistManager.java
+++ src/BLL/SongOnPlaylistManager.java
@@ -2,12 +2,10 @@ package src.BLL;
 
 import src.BE.Playlist;
 import src.BE.Song;
-import src.BE.SongOnPlaylist;
 import src.DAL.db.ISongOnPlaylistDataAccess;
 import src.DAL.db.SongOnPlaylistDAO;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SongOnPlaylistManager {
 
@@ -17,23 +15,39 @@ public class SongOnPlaylistManager {
         songOnPlaylistDAO = new SongOnPlaylistDAO();
     }
 
-    public SongOnPlaylistManager(ISongOnPlaylistDataAccess songOnPlaylistDAO) {
-        this.songOnPlaylistDAO = songOnPlaylistDAO;
-    }
+    /**
+     * This constructor initializes a new instance of the SongOnPlaylistManager class with the specified.
+     * @param songOnPlaylistDAO
+     */
+    public SongOnPlaylistManager(ISongOnPlaylistDataAccess songOnPlaylistDAO) {this.songOnPlaylistDAO = songOnPlaylistDAO;}
 
+    /**
+     * This method adds a song to a given playlist and returns the added song.
+     * @param playlist
+     * @param song
+     * @return songOnPlaylistDAO.addToPlaylist(playlist, song).
+     * @throws Exception
+     */
+    public Song addToPlaylist(Playlist playlist, Song song) throws Exception {return songOnPlaylistDAO.addToPlaylist(playlist, song);}
 
-    public Song addToPlaylist(Playlist playlist, Song song) throws Exception
+    /**
+     * This method retrieves a list of songs associated with a given playlist.
+     * @param playlist
+     * @return songOnPlaylistDAO.getSongsOnPlaylist(playlist)
+     */
+    // This method retrieves a list of songs associated with a given playlist.
+    public ArrayList<Song> getSongsOnPlaylist(Playlist playlist )
+    // Use the songOnPlaylistDAO object to retrieve the list of songs from the data source.
+    {return songOnPlaylistDAO.getSongsOnPlaylist(playlist);}
 
-    {
-        return songOnPlaylistDAO.addToPlaylist(playlist, song);
-    }
-
-    public ArrayList<Song> getSongsOnPlaylist(Playlist playlist ) {
-        return songOnPlaylistDAO.getSongsOnPlaylist(playlist);
-    }
+    /**
+     *The method takes two parameters: a Playlist object and a Song object.
+     * It then uses the songOnPlaylistDAO object to call its deleteSongOnPlaylist method,
+     * passing the playlist and song objects as arguments.
+     * @param playlist
+     * @param song
+     */
     public void deleteSongOnPlaylist(Playlist playlist, Song song)
-    {
-        songOnPlaylistDAO.deleteSongOnPlaylist(playlist, song);
-    }
+    {songOnPlaylistDAO.deleteSongOnPlaylist(playlist, song);}
 
 }
